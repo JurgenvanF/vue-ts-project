f1daa29b6efc1e1c3ec1228f748afe724fb44e68
JurgenvanF <frae0004@hz.nl>
2026-03-30 00:59:51 +0200

    add a footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import Navbar from "./components/navbar/Navbar.vue";
+import Footer from "./components/footer/Footer.vue";
 </script>
 
 <template>
-  <div class="navbar">
-    <Navbar />
-  </div>
-  <div class="content">
-    <router-view />
+  <div class="app">
+    <div class="navbar">
+      <Navbar />
+    </div>
+    <div class="content">
+      <router-view />
+    </div>
+    <div class="footer-container">
+      <Footer />
+    </div>
   </div>
 </template>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import "./Footer.scss";
+</script>
+
+<template>
+  <div class="footer">
+    <p>© 2026 Test Company. All rights reserved.</p>
+  </div>
+</template>
